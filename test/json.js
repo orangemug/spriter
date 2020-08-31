dd39cb2bd2a3644d8ec758c8e7899e7f37acea84
orangemug <info@orangemug.co.uk>
2020-08-31 09:54:14 +0100

    Round width/height to integers.

--- test/json.js
+++ test/json.js
@@ -95,5 +95,52 @@ describe("json", () => {
       pixelRatio: 2
     });
   });
+
+  it("rounded dimensions", async () => {
+    const imgs = [
+      {
+        "id": "foo",
+        "url": "http://example.com/foo.png",
+        "width": 30.4,
+        "height": 80.4,
+      },
+      {
+        "id": "bar",
+        "url": "http://example.com/bar.png",
+        "width": 49.9,
+        "height": 39.9,
+      },
+    ];
+
+    const result = spriter.json(imgs);
+
+    assert.deepStrictEqual(result, {
+      width: 50,
+      height: 120,
+      images: [
+        {
+          id: 'foo',
+          url: 'http://example.com/foo.png',
+          width: 30,
+          height: 80,
+          x: 0,
+          y: 0
+        },
+        {
+          id: 'bar',
+          url: 'http://example.com/bar.png',
+          width: 50,
+          height: 40,
+          x: 0,
+          y: 80
+        }
+      ],
+      boxes: {
+        foo: { pixelRatio: 1, width: 30, height: 80, x: 0, y: 0 },
+        bar: { pixelRatio: 1, width: 50, height: 40, x: 0, y: 80 }
+      },
+      pixelRatio: 1
+    });
+  });
 });
 
